2ee691d62a17aa4b0861037a7840171b1436dbdd
corey <ctomames@gmail.com>
2026-01-05 23:43:14 -0500

    worked on simple grid layout, place player div in

--- ui/ui.js
+++ ui/ui.js
@@ -1,4 +1,17 @@
+const playerDivStr = '<div class="player"></div>';
+const playerDiv = document.createElement('div')
+playerDiv.innerHTML = playerDivStr;
+
+document.onreadystatechange = () => {
+    if (document.readyState == 'interactive') {
+        //console.log(document.getElementsByClassName("row0").item(0).childNodes.item(0));
+        console.log(document.getElementsByClassName("row0").item(0).childNodes[1].appendChild(playerDiv));
+        //console.log(output);
+        
+        //document.getElementsByClassName("row0").item(0);
 
 
 
+    }
+}
 
