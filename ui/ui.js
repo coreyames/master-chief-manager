b7c95ed9e58f101034dfa8e3f64a52cc24c0b0a0
corey <ctomames@gmail.com>
2026-01-06 13:20:56 -0500

    update, clean up coords

--- ui/ui.js
+++ ui/ui.js
@@ -1,14 +1,24 @@
 const playerDivStr = '<div class="player"></div>';
 const playerDiv = document.createElement('div')
 playerDiv.innerHTML = playerDivStr;
+const a2x = { 'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4,
+    'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9
+};
+const x2a = Object.fromEntries(Object.entries(a2x).map(([k,v]) => {
+    return [v,k];
+}));
 
 document.onreadystatechange = () => {
     if (document.readyState == 'interactive') {
-        const e = document.getElementsByClassName('row0').item(0).firstElementChild
+        const e = document.getElementsByClassName('colA').item(0).firstElementChild
         e.insertBefore(playerDiv, e.firstChild); 
     }
-}
+};
 
-// dn = document.createElement(playerDivStr);
+const movePlayer = (x, y) => {
+
+};
+
+
 
 
