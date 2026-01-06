a235d24bceaabb4ac140dd8ec3237c61056caaca
corey <ctomames@gmail.com>
2026-01-06 15:20:24 -0500

    move player icon working

--- ui/ui.js
+++ ui/ui.js
@@ -1,24 +1,19 @@
 const playerDivStr = '<div class="player"></div>';
 const playerDiv = document.createElement('div')
 playerDiv.innerHTML = playerDivStr;
-const a2x = { 'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4,
-    'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9
-};
-const x2a = Object.fromEntries(Object.entries(a2x).map(([k,v]) => {
-    return [v,k];
-}));
+
+const player = {};
+player.x, player.y = 0, 0;
+
 
 document.onreadystatechange = () => {
     if (document.readyState == 'interactive') {
-        const e = document.getElementsByClassName('colA').item(0).firstElementChild
-        e.insertBefore(playerDiv, e.firstChild); 
+        movePlayer(0, 0);
     }
 };
 
 const movePlayer = (x, y) => {
-
+    const e = document.getElementsByClassName('col').item(x).getElementsByClassName('row').item(9-y);
+    console.log(e);
+    e.insertBefore(playerDiv, e.firstChild);      
 };
-
-
-
-
