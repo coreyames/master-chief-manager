0b3a847121f302cee6e81329eadac4d99e6bce02
corey <ctomames@gmail.com>
2026-01-06 15:46:56 -0500

    onclick move

--- ui/ui.js
+++ ui/ui.js
@@ -5,15 +5,20 @@ playerDiv.innerHTML = playerDivStr;
 const player = {};
 player.x, player.y = 0, 0;
 
-
 document.onreadystatechange = () => {
     if (document.readyState == 'interactive') {
         movePlayer(0, 0);
+        const onClick = (e) => {
+            movePlayer(5, 5);
+        };
+        addEventListener("click", onClick); 
     }
 };
 
 const movePlayer = (x, y) => {
-    const e = document.getElementsByClassName('col').item(x).getElementsByClassName('row').item(9-y);
-    console.log(e);
-    e.insertBefore(playerDiv, e.firstChild);      
+    const prev = document.getElementsByClassName('player');
+    const el = document.getElementsByClassName('col').item(x).getElementsByClassName('row').item(9-y);
+    el.insertBefore(playerDiv, el.firstChild);
+    if (prev.parentElement) prev.parentElement.removeChild(prev);
+    player.x = x; player.y = y;      
 };
