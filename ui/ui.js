e66d86fd80634e3cb87698ac8894fe849c8baf4d
corey <ctomames@gmail.com>
2026-01-07 07:46:06 -0500

    discrete point move in 100 by 100

--- ui/ui.js
+++ ui/ui.js
@@ -1,6 +1,5 @@
-const playerDivStr = '<div class="player"></div>';
 const playerDiv = document.createElement('div')
-playerDiv.innerHTML = playerDivStr;
+playerDiv.className = 'player';
 
 const player = {};
 player.x, player.y = 0, 0;
@@ -8,23 +7,39 @@ player.parentElement = null;
 
 document.onreadystatechange = () => {
     if (document.readyState == 'interactive') {
-        movePlayer(0, 0);
+        
+        const b = document.getElementsByTagName('BODY').item(0);        
+        b.insertBefore(playerDiv, b.firstChild);
         const clickToMove = (e) => {
             console.log(e);
             if (e.target.tagName == 'BODY') return;
             e.target.insertBefore(playerDiv, e.target.firstChild);
-            player.parentElement = e.target;             
+            movePlayerToOffset(e);
+            player.parentElement = e.target.parentElement;             
         };
+        
         addEventListener("click", clickToMove); 
     }
 };
 
-const movePlayer = (x, y) => {
+const movePlayerToCell = (x, y) => {
     const el = document.getElementsByClassName('col').item(x).getElementsByClassName('row').item(9-y);
     el.insertBefore(playerDiv, el.firstChild);
     player.x = x; player.y = y;      
 };
 
-const removePlayer = () => {
-   playerDiv.parentElement.removeChild(playerDiv);
+const movePlayerToOffset = (e) => {
+    console.log(e.offsetX)
+    console.log(e.offsetY)
+    playerDiv.style.left = e.offsetX;
+    playerDiv.style.top = e.offsetY;
 }
+
+const movePlayer = (e) => {
+
+}
+
+const removePlayer = () => {
+    playerDiv.parentElement.removeChild(playerDiv);
+}
+
