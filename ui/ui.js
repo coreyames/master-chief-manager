d3733854ddd979e4b4ae073df01b1027c93eea78
corey <ctomames@gmail.com>
2026-01-08 01:41:27 -0500

    added moveplayer the combines movetocell and offset to take 0 <= x,y <= 99 from any source and place

--- ui/ui.js
+++ ui/ui.js
@@ -1,21 +1,23 @@
 const playerDiv = document.createElement('div')
 playerDiv.className = 'player';
 
-const player = {};
-player.x, player.y = 0, 0;
-player.parentElement = null;
+const pxRatio = 6; 
 
 document.onreadystatechange = () => {
     if (document.readyState == 'interactive') {
         
         const b = document.getElementsByTagName('BODY').item(0);        
         b.insertBefore(playerDiv, b.firstChild);
+       
         const clickToMove = (e) => {
+            /*
             console.log(e);
             if (e.target.tagName == 'BODY') return;
             e.target.insertBefore(playerDiv, e.target.firstChild);
             movePlayerToOffset(e);
-            player.parentElement = e.target.parentElement;             
+            */
+            const coords = { x: 55, y: 55 };
+            movePlayer(coords.x, coords.y);
         };
         
         addEventListener("click", clickToMove); 
@@ -25,14 +27,21 @@ document.onreadystatechange = () => {
 const movePlayerToCell = (x, y) => {
     const el = document.getElementsByClassName('col').item(x).getElementsByClassName('row').item(9-y);
     el.insertBefore(playerDiv, el.firstChild);
-    player.x = x; player.y = y;      
 };
 
 const movePlayerToOffset = (e) => {
-    console.log(e.offsetX)
-    console.log(e.offsetY)
+    console.log(e);
     playerDiv.style.left = e.offsetX;
     playerDiv.style.top = e.offsetY;
+}
+
+const movePlayer = (x_, y_) => {
+    const x = parseInt(x_ / 10); 
+    const y = parseInt(y_ / 10);
+    const offsetX = x_ % 10; 
+    const offsetY = y_ % 10; 
+    movePlayerToCell(x, y);
+    movePlayerToOffset({offsetX: offsetX*pxRatio, offsetY: offsetY*pxRatio});
 }
 
 const removePlayer = () => {
