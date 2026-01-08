30b52560a7c6f935873def423cf2b19ab06956b9
corey <ctomames@gmail.com>
2026-01-08 11:47:40 -0500

    dot now visually centered on point

--- ui/ui.js
+++ ui/ui.js
@@ -1,7 +1,16 @@
 const playerDiv = document.createElement('div')
 playerDiv.className = 'player';
+playerDiv.style.position = 'fixed'
 
-const pxRatio = 6; 
+const pxRatio = 6;
+const centeringOffsetX = 7;
+const centeringOffsetY = 8;
+
+const player = {
+    x: 0, y: 0, // abstract coords 0-99, 0,99
+    row: 0 , col: 0, // index of row in grid (grows top down)
+    offsetX: 0, offsetY: 0 // px offset (for visual, values not coords) for grid cell
+};
 
 document.onreadystatechange = () => {
     if (document.readyState == 'interactive') {
@@ -27,21 +36,30 @@ document.onreadystatechange = () => {
 const movePlayerToCell = (x, y) => {
     const el = document.getElementsByClassName('col').item(x).getElementsByClassName('row').item(9-y);
     el.insertBefore(playerDiv, el.firstChild);
+    player.row = 9-y; 
+    player.col = x;
 };
 
-const movePlayerToOffset = (e) => {
-    console.log(e);
+const movePlayerToOffset = (e) => {   
     playerDiv.style.left = e.offsetX;
-    playerDiv.style.top = e.offsetY;
+    playerDiv.style.top  = e.offsetY;
+
+    player.offsetX = e.offsetX;
+    player.offsetY = e.offsetY;
 }
 
 const movePlayer = (x_, y_) => {
+    playerDiv.style.position = 'relative';
+    player.x = x_;
+    player.y = y_;
     const x = parseInt(x_ / 10); 
     const y = parseInt(y_ / 10);
     const offsetX = x_ % 10; 
     const offsetY = y_ % 10; 
+    const adjusted_offsetX = (pxRatio * offsetX) - centeringOffsetX; 
+    const adjusted_offsetY = (pxRatio * offsetY) - centeringOffsetY; 
     movePlayerToCell(x, y);
-    movePlayerToOffset({offsetX: offsetX*pxRatio, offsetY: offsetY*pxRatio});
+    movePlayerToOffset({offsetX: adjusted_offsetX , offsetY: adjusted_offsetY});
 }
 
 const removePlayer = () => {
